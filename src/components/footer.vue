5be4de996f658f4a0ca6eff60dff947c28f116bc
Fanyi-Meng <fanyim@Fanyi-MengdeMBP.lan>
2018-08-21 00:23:44 +0800

    navbar&footer

---
+++ src/components/footer.vue
@@ -0,0 +1,22 @@
+<template>
+<footer class="ffooter">
+    <div class="copy">
+        Friends Mind &copy; 2018
+    </div>
+</footer>
+</template>
+<script>
+export default {
+    name: 'ffooter'
+}
+</script>
+<style lang="scss">
+.ffooter{
+    position: fixed;
+    bottom: 0%; left: 0;
+    width: 100%;
+    background-color: #aaaaaa;
+    display: flex;
+    align-content: center;
+}
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,24 @@
+<template>
+<nav class="fnav">
+    <div class="brand nav-left">
+        <img src="@/assets/logo.png" alt="logo" height="20px">
+        <span class="title">FriendsMind</span>
+    </div>
+</nav>
+</template>
+<script>
+export default {
+    name: 'fnav',
+}
+</script>
+<style lang="scss">
+
+.fnav{
+    position: fixed;
+    width: 100%;
+    top: 0; left: 0;
+    background-color: #f4dd5a;
+    display: flex;
+    align-items: flex-start;
+}
+</style>
